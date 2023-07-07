abc5cc7e9b8a083a51fb2ea45d8c28a422f80aa5
Craig Falkner <craigtfalkner@gmail.com>
2023-07-07 01:00:55 -0600

    Removed lag from get_stats functions, added some team data.

--- get_stats.py
+++ get_stats.py
@@ -35,11 +35,8 @@ def get_hitting(start_season = 1876, end_season = current_year):
         temp_df = pd.DataFrame(make_request['stats'])
         hitting_df = pd.concat([hitting_df, temp_df], axis = 0)
         print(f'Finished {y} season.')
-        lag = np.random.uniform(low = 2, high = 5)
-        print(f'Waiting {round(lag, 1)} seconds before getting next season.')
-        time.sleep(lag)
     # Data saved as csv in current working directory
-    hitting_df.to_csv(current_directory + '\\hitting_data.csv')
+    hitting_df.to_csv(current_directory + '\\data\\hitting_data.csv')
 
 def get_pitching(start_season = 1876, end_season = current_year):
     years = list(map(str, range(start_season, end_season + 1)))
@@ -54,11 +51,8 @@ def get_pitching(start_season = 1876, end_season = current_year):
         temp_df = pd.DataFrame(make_request['stats'])
         pitching_df = pd.concat([pitching_df, temp_df], axis = 0)
         print(f'Finished {y} season.')
-        lag = np.random.uniform(low = 2, high = 5)
-        print(f'Waiting {round(lag, 1)} seconds before getting next season.')
-        time.sleep(lag)
     # Data saved as csv in current working directory
-    pitching_df.to_csv(current_directory + '\\pitching_data.csv')
+    pitching_df.to_csv(current_directory + '\\data\\pitching_data.csv')
 
 def get_war(start_season = 1871, end_season = current_year):
     years = list(map(str, range(start_season, end_season + 1)))
@@ -68,16 +62,13 @@ def get_war(start_season = 1871, end_season = current_year):
     for y in years:
         if break_program:
             break
-        api_url = 'https://www.fangraphs.com/api/leaders/war?season=' + y + '&lg=&wartype=0&teamid='
+        api_url = f'https://www.fangraphs.com/api/leaders/war?season={y}&lg=&wartype=0&teamid='
         make_request = requests.get(url = api_url).json()
         temp_df = pd.DataFrame(make_request)
         war_df = pd.concat([war_df, temp_df], axis = 0)
         print(f'Finished {y} season.')
-        lag = np.random.uniform(low = 2, high = 5)
-        print(f'Waiting {round(lag, 1)} seconds before getting next season.')
-        time.sleep(lag)
     # Data saved as csv in current working directory
-    war_df.to_csv(current_directory + '\\war_data.csv')
+    war_df.to_csv(current_directory + '\\data\\war_data.csv')
 
 def get_contracts():
     contracts_df = pd.DataFrame()
@@ -91,14 +82,11 @@ def get_contracts():
         temp_df1 = pd.DataFrame(make_request['players'])
         temp_df2 = pd.json_normalize(temp_df1['contractSummary'])
         contracts_df = pd.concat([contracts_df, temp_df2], axis=0)
-        print(f'Finished {t}.')
-        lag = np.random.uniform(low = 2, high = 5)
-        print(f'Waiting {round(lag, 1)} seconds before getting next season.')
-        time.sleep(lag)
+        print(f'Finished team {t}.')
     # Data saved as csv in current working directory
-    contracts_df.to_csv(current_directory + '\\contract_data.csv')
+    contracts_df.to_csv(current_directory + '\\data\\contract_data.csv')
 
 #get_pitching()
 #get_hitting()
 #get_war()
-#get_contracts()
+get_contracts()
